d38db469a7b2bab5e10a61c9bb0c51311f015fbe
unikmo <mbanwie@googlemail.com>
2026-09-10 20:20:37 +0200

    Use the current public literacy bank consistently

--- lib/deep-bank.ts
+++ lib/deep-bank.ts
@@ -1,8 +1,9 @@
-import { deepBeliefQuestions, deepLiteracyQuestions } from './deep-questions';
+import { deepBeliefQuestions } from './deep-questions';
 import { deepDiscriminatorQuestions } from './deep-discriminators';
+import { literacyQuestions } from './literacy-questions';
 
-export const DEEP_QUESTIONNAIRE_VERSION = 'deep-2026.09-v1.2' as const;
-export { deepLiteracyQuestions };
+export const DEEP_QUESTIONNAIRE_VERSION = 'deep-2026.09-v1.3-reader' as const;
+export const deepLiteracyQuestions = literacyQuestions;
 
 export const allDeepBeliefQuestions = [...deepBeliefQuestions, ...deepDiscriminatorQuestions] as const;
 export const allDeepQuestions = [...allDeepBeliefQuestions, ...deepLiteracyQuestions] as const;
