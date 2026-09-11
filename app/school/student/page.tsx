c62860b98db4d73441b603a8ec90365d2aa24c09
unikmo <mbanwie@googlemail.com>
2026-09-11 13:02:06 +0200

    Localize Student School page label

--- app/school/student/page.tsx
+++ app/school/student/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import StudentSchoolClient from './StudentSchoolClient';
 import { LanguageSelector } from '../../LocaleProvider';
+import SchoolRoleLabel from '../SchoolRoleLabel';
 
 export default async function SchoolStudentPage({ searchParams }: { searchParams: Promise<{ code?: string }> }) {
   const params = await searchParams;
@@ -8,7 +9,7 @@ export default async function SchoolStudentPage({ searchParams }: { searchParams
     <main className="engine-page">
       <header className="engine-header">
         <Link href="/school" className="engine-brand">Politangle School</Link>
-        <span>Student</span><LanguageSelector />
+        <SchoolRoleLabel role="student" /><LanguageSelector />
       </header>
       <StudentSchoolClient initialCode={params.code ?? ''} />
     </main>
