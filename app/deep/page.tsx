ec12c475d75a59df5ed724a148a2b477922a7f6f
unikmo <mbanwie@googlemail.com>
2026-09-12 10:03:41 +0200

    Use native Full route header

--- app/deep/page.tsx
+++ app/deep/page.tsx
@@ -1,14 +1,10 @@
-import Link from 'next/link';
+import AssessmentHeader from '../AssessmentHeader';
 import DeepClient from './DeepClient';
-import { LanguageSelector } from '../LocaleProvider';
 
 export default function DeepPage() {
   return (
     <main className="engine-page">
-      <header className="engine-header">
-        <Link href="/" className="engine-brand">Politangle</Link>
-        <span>Full · 16 more questions</span><LanguageSelector />
-      </header>
+      <AssessmentHeader kind="full" />
       <DeepClient />
     </main>
   );
