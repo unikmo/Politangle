99f2bb67496fd6776b92007efe3e77bf88612859
unikmo <mbanwie@googlemail.com>
2026-09-12 10:08:19 +0200

    Use localized teacher header

--- app/school/teacher/page.tsx
+++ app/school/teacher/page.tsx
@@ -1,14 +1,10 @@
-import Link from 'next/link';
+import TeacherHeader from './TeacherHeader';
 import TeacherLicenseGate from './TeacherLicenseGate';
-import { LanguageSelector } from '../../LocaleProvider';
 
 export default function SchoolTeacherPage() {
   return (
     <main className="engine-page">
-      <header className="engine-header">
-        <Link href="/school" className="engine-brand">Politangle School</Link>
-        <span>Teacher</span><LanguageSelector />
-      </header>
+      <TeacherHeader />
       <TeacherLicenseGate />
     </main>
   );
