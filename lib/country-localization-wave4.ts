69766826178bbad49654b029fb28c1adf95e6b4d
unikmo <mbanwie@googlemail.com>
2026-09-21 01:08:56 +0300

    Assemble localized country perspectives 61-80

---
+++ lib/country-localization-wave4.ts
@@ -0,0 +1,4 @@
+export { deWave4 } from './country-localization-wave4-de';
+export { esWave4 } from './country-localization-wave4-es';
+export { frWave4 } from './country-localization-wave4-fr';
+export { ptBrWave4 } from './country-localization-wave4-ptbr';
